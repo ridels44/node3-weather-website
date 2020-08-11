26d9495e0211c81f72a3b9edef9d9e69510e6bd1
ridels44 <rambingridels@gmail.com>
2020-08-11 21:57:39 +0800

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,16 +11,14 @@ weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const location = search.value;
 
-  fetch("http://localhost:3000/weather?address=" + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          message1.textContent = data.error;
-        } else {
-          message1.textContent = `${data.location}`;
-          message2.textContent = `temperature : ${data.temperature} , wind speed : ${data.wind_speed}`;
-        }
-      });
-    }
-  );
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        message1.textContent = data.error;
+      } else {
+        message1.textContent = `${data.location}`;
+        message2.textContent = `temperature : ${data.temperature} , wind speed : ${data.wind_speed}`;
+      }
+    });
+  });
 });
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
 const app = express();
+const port = process.env.PORT() || 3000;
 
 // define paths for express config
 const publicDirPath = path.join(__dirname, "../public");
@@ -34,7 +35,7 @@ app.get("/weather", (req, res) => {
     });
   }
 
-  // geocode / geolocation
+  // geocode / forecast
   geocode(
     req.query.address,
     (error, { longitude, latitude, location } = {}) => {
@@ -99,6 +100,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("server is up on port 3000");
+app.listen(port, () => {
+  console.log("server is up on port 3000" + port);
 });
